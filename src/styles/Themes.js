a289cda7ff3a50ee8b5733b4746244ae97dff0e5
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-19 13:20:21 -0500

    setup color theme

---
+++ src/styles/Themes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    mainBrand: "#f04d54",
+    lightShade: "#F5F6F4",
+    lightAccent: "#9CA4AF",
+    darkAccent: "#D88958",
+    darkShade: "#343540",
+  },
+  fonts: ["sans-serif", "Roboto"],
+  fontSizes: {
+    small: "1em",
+    medium: "2em",
+    large: "3em",
+  },
+};
+
+const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+export default Theme;
