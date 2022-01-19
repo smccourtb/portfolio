7a3b1dd527c21ba88cb62341495813c41ea48578
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-19 13:21:17 -0500

    add media queries to scale font size

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -20,7 +20,25 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+
+  }
+
+  html, body {
+    scroll-snap-type: y mandatory;
     font-size: 15px;
+    @media (min-width: 600px) {
+      font-size: 24px;
+    }
+    @media (min-width: 960px) {
+      font-size: 36px;
+    }
+    @media (min-width: 1280px) {
+      font-size: 48px;
+    }
+    @media (min-width: 1920px) {
+      font-size: 56px;
+    }
   }
 
   @font-face {
