4bb1d157239af6de3ea5a038260dc31ae012d9a8
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-21 13:57:11 -0500

    add data prop to Projects component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import styled from "styled-components/macro";
-import { Greeting } from "./components/Greeting";
-import { Header } from "./styles/shared-styles";
 import Theme from "./styles/Themes";
+
+import { Greeting } from "./components/Greeting";
 import { AboutMe } from "./components/AboutMe";
 import { Projects } from "./components/Projects";
-
-export const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  color: ${({ primary, theme: { colors } }) =>
-    primary ? colors.lightShade : colors.darkShade};
-  background-color: ${({ primary, theme: { colors } }) =>
-    primary ? colors.darkShade : colors.lightShade};
-  display: flex;
-  flex-direction: ${({ column }) => (column ? "column" : "row")};
-  align-items: ${({ center }) => (center ? "center" : "flex-start")};
-  scroll-snap-align: start;
-  padding: 24px;
-  font-size: 1em;
-`;
+import { Contact } from "./components/Contact";
+import projectsData from "./ProjectsData";
 
 function App() {
   return (
     <Theme>
       <Greeting />
       <AboutMe />
-      <Projects />
+      <Projects data={projectsData} />
+      <Contact />
     </Theme>
   );
 }
