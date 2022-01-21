d7d978c77109b467c6f9ee729dc3055461d8b9aa
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-21 13:52:39 -0500

    move container styled component to global styles

--- src/styles/shared-styles.js
+++ src/styles/shared-styles.js
@@ -5,3 +5,19 @@ export const Header = styled.h2`
   font-weight: 700;
   font-size: 36px;
 `;
+
+export const Container = styled.section`
+  height: ${({ fullPage }) => (fullPage ? "100vh" : "unset")};
+  min-height: 100vh;
+  width: 100%;
+  color: ${({ primary, theme: { colors } }) =>
+    primary ? colors.lightShade : colors.darkShade};
+  background-color: ${({ primary, theme: { colors } }) =>
+    primary ? colors.darkShade : colors.lightShade};
+  display: flex;
+  flex-direction: ${({ column }) => (column ? "column" : "row")};
+  align-items: ${({ center }) => (center ? "center" : "flex-start")};
+  scroll-snap-align: start;
+  padding: 24px;
+  font-size: 1em;
+`;
