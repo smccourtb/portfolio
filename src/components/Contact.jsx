a821283ef0de59620a7f99d0a9fb8d1ffbf58be6
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-21 13:53:15 -0500

    create contact page stub

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+import { Container, Header } from "../styles/shared-styles";
+
+export const Contact = () => {
+  return (
+    <Container>
+      <Header>Contact Me</Header>
+    </Container>
+  );
+};
