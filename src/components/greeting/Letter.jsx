d11d4130eb3a3ba60c8e0f64d253ae86ab62d767
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-02-05 15:15:23 -0500

    refactor Letter component styles to letter-styles.js

--- src/components/greeting/Letter.jsx
+++ src/components/greeting/Letter.jsx
@@ -1,31 +1,6 @@
 import styled, { keyframes, css } from "styled-components/macro";
 import { useState } from "react";
-
-const squash = keyframes`
-  10% {
-    transform: scale(130%, 20%);
-  }
-  50% {
-    transform: scale(110%, 150%);
-  }
-  65% {
-    transform: scale(90%, 125%)
-  }
-  100% {
-    transform: scale(100%);
-  }
-`;
-
-const GreetingLetter = styled.span`
-  font-family: "Paytone One", serif;
-  display: inline-block;
-  color: ${({ playIntro, theme: { colors } }) => playIntro && colors.mainBrand};
-  animation: ${({ playIntro }) =>
-    playIntro &&
-    css`
-      ${squash} .7s ease-in-out 1
-    `};
-`;
+import { GreetingLetter } from "../../styles/letter-styles";
 
 export const Letter = ({ letter }) => {
   const [hovered, setHovered] = useState(false);
