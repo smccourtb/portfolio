bff68c1665a5df0494fdd563c7761b0ee500e283
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-02-05 15:15:58 -0500

    refactor Greeting component styles to greeting-styles.js

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,4 +1,3 @@
-import styled, { keyframes } from "styled-components/macro";
 import { Letter } from "./greeting/Letter";
 import { useState } from "react";
 import { Typing } from "./greeting/Typing";
@@ -15,8 +14,13 @@ import {
 } from "../styles/greeting-styles";
 
 export const Greeting = () => {
+  // used to detect when opening animation is complete
   const [state, setState] = useState(false);
+
+  // used to track mouse movement for the 3D word effect
   const [movement, setMovement] = useState([]);
+
+  // creates the jiggly letters on mouse over
   const introLetters = (text) =>
     text.split("").map((letter, idx) => <Letter key={idx} letter={letter} />);
 
@@ -39,26 +43,23 @@ export const Greeting = () => {
         </Sentence>
 
         {state ? (
-          <div style={{ display: "flex" }}>
+          <FlexContainer>
             <Pronunciation delay={".1s"}>{introLetters("Shaw")}</Pronunciation>
             <Pronunciation delay={"1.1s"}>{introLetters("na")}</Pronunciation>
             <Pronunciation delay={"2.1s"}>
               {introLetters("than.")}
             </Pronunciation>
-          </div>
+          </FlexContainer>
         ) : (
           <Typing setState={setState} />
         )}
 
         <HandWritingContainer>
-          <DrawnPronunciation
-            style={{ alignSelf: "flex-start" }}
-            delay={"8.4s"}
-          >
+          <DrawnPronunciation left delay={"8.4s"}>
             Shaw
           </DrawnPronunciation>
           <DrawnPronunciation delay={"9.4s"}>nuh</DrawnPronunciation>
-          <DrawnPronunciation style={{ alignSelf: "flex-end" }} delay={"10.4s"}>
+          <DrawnPronunciation right delay={"10.4s"}>
             then
           </DrawnPronunciation>
         </HandWritingContainer>
---
+++ src/styles/greeting-styles.js
@@ -0,0 +1,88 @@
+import styled, { keyframes } from "styled-components/macro";
+
+const Pronounce = keyframes`
+  from {
+    color: inherit;
+  }
+  to {
+    color: #f04d54
+  }
+`;
+
+const drawnPronounce = keyframes`
+  100% {
+    opacity: 100%;
+  }
+`;
+
+const Splay = keyframes`
+  100% {
+    opacity: 5%;
+    font-size: 3em;
+  }
+`;
+
+const FadeOut = keyframes`
+  100% {
+    opacity: 0;
+  }
+`;
+
+export const OpeningHeader = styled.div`
+  font-family: "Paytone One", serif;
+  font-size: 3em;
+  width: 100%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0.5em;
+  font-weight: bold;
+`;
+
+export const Pronunciation = styled.div`
+  animation: ${Pronounce} 0.7s ease-out;
+  animation-delay: ${({ delay }) => delay};
+`;
+
+export const DrawnPronunciation = styled.div`
+  font-family: "Rock Salt", serif;
+  animation: ${drawnPronounce} 0.7s cubic-bezier(1, -0.09, 0.79, 0.98) forwards;
+  animation-delay: ${({ delay }) => delay};
+  opacity: 0;
+  position: relative;
+  align-self: ${({ left, right }) =>
+    left ? "flex-start" : right ? "flex-end" : "center"};
+`;
+
+export const Trademark = styled(DrawnPronunciation)`
+  position: absolute;
+  top: 30%;
+  left: 0;
+  right: 0;
+  animation: ${Splay} 0.7s cubic-bezier(0.26, -0.95, 1, -0.54) forwards;
+  animation-delay: ${({ delay }) => delay};
+  transform: scale(10);
+`;
+
+export const HandWritingContainer = styled.div`
+  font-size: 1em;
+  opacity: 5%;
+  display: flex;
+  flex-direction: column;
+  line-height: 1.25em;
+  width: 100%;
+  max-width: 500px;
+  align-self: center;
+  padding: 0.5em;
+  animation: ${FadeOut} 1s 12s forwards cubic-bezier(0.26, -0.95, 1, -0.54);
+`;
+
+export const Sentence = styled.div`
+  display: flex;
+  gap: 0.2em;
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+`;
