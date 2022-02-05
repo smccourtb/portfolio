d2639c31c5e8da22c985df7783a98f7b57e244ed
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-02-05 15:19:12 -0500

    add hobby and more tech icons

--- src/components/projects/DevelopmentIcon.jsx
+++ src/components/projects/DevelopmentIcon.jsx
@@ -6,7 +6,23 @@ import {
   SiStyledcomponents,
   SiJavascript,
   SiGodotengine,
+  SiWebstorm,
+  SiVisualstudiocode,
+  SiWebpack,
+  SiFirebase,
+  SiTrello,
+  SiGit,
+  SiGithub,
+  SiGitkraken,
 } from "react-icons/si";
+
+import {
+  FaGuitar,
+  FaHiking,
+  FaDiceD20,
+  FaGamepad,
+  FaBiking,
+} from "react-icons/fa";
 
 const DevLogo = styled.div`
   cursor: pointer;
@@ -27,11 +43,42 @@ const getLogo = (techName, size) => {
       return <SiJavascript size={size} />;
     case "GDScript":
       return <SiGodotengine size={size} />;
+    case "Webstorm":
+      return <SiWebstorm size={size} />;
+    case "Webpack":
+      return <SiWebpack size={size} />;
+    case "Visual Studio Code":
+      return <SiVisualstudiocode size={size} />;
+    case "Trello":
+      return <SiTrello size={size} />;
+    case "Firebase":
+      return <SiFirebase size={size} />;
+    case "Git":
+      return <SiGit size={size} />;
+    case "GitHub":
+      return <SiGithub size={size} />;
+    case "GitKraken":
+      return <SiGitkraken size={size} />;
+    case "Play Guitar":
+      return <FaGuitar size={size} />;
+    case "Go Hiking":
+      return <FaHiking size={size} />;
+    case "Go for a Bike Ride":
+      return <FaBiking size={size} />;
+    case "Play Board Games":
+      return <FaDiceD20 size={size} />;
+    case "Develop Video Games":
+      return <FaGamepad size={size} />;
+
     default:
       return;
   }
 };
 
 export const DevelopmentIcon = ({ name, size }) => {
-  return <DevLogo name={name}>{getLogo(name, size)}</DevLogo>;
+  return (
+    <abbr title={name}>
+      <DevLogo>{getLogo(name, size)}</DevLogo>
+    </abbr>
+  );
 };
