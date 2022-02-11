4cf5e537bae1b0eee304b6374034c33066604652
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-02-10 22:59:13 -0500

    wrap filter and shuffle into useCallback hook and remove useEffect hook

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,5 @@
 import { Project } from "./projects/Project";
-import { Flipper, Flipped } from "react-flip-toolkit";
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import FilterButton from "./projects/FilterButton";
 import { FlexContainer, Header, variants } from "../styles/global/GlobalStyles";
 import { DevelopmentIcon } from "./projects/DevelopmentIcon";
@@ -13,53 +12,38 @@ import {
   ProjectsContainer,
 } from "../styles/projects-styles";
 
+const tools = ["React", "Javascript", "GDScript"];
+
 export const Projects = ({ data }) => {
   const [projectData, setProjectData] = useState(data);
   const [settings, setSettings] = useState([]);
 
-  const shuffle = () => {
-    const sorted = projectData.sort(() => (Math.random() > 0.5 ? 1 : -1));
-    setProjectData([...sorted]);
-  };
-
-  useEffect(() => {
-    const newData = [...data];
-    const filtered = newData.filter((element) => {
-      for (const i of settings) {
-        if (element.tag === i) {
-          return true;
-        }
-      }
-      return false;
-    });
-    if (filtered.length > 0) {
-      setProjectData(filtered);
-    } else {
-      setProjectData(newData);
+  const shuffle = useCallback(() => {
+    const newData = [...projectData];
+    for (let i = newData.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newData[i], newData[j]] = [newData[j], newData[i]];
     }
-  }, [data, settings]);
+    setProjectData(newData);
+  }, [projectData]);
 
-  const projects = projectData.map((project) => {
-    return (
-      <Flipped flipId={project.id} key={project.id}>
-        <Div>
-          <Project projectData={project} key={project.id} />
-        </Div>
-      </Flipped>
-    );
-  });
+  const filter = useCallback(
+    (x) => {
+      const newSettings = [...settings];
+      // check if target is in the array, add it if not, remove it if so
+      newSettings.includes(x)
+        ? newSettings.splice(newSettings.indexOf(x), 1)
+        : newSettings.push(x);
 
-  const filterButtons = () => {
-    const tools = ["React", "Javascript", "GDScript"];
-    return tools.map((tool, idx) => (
-      <FilterButton
-        key={idx}
-        setSettings={setSettings}
-        settings={settings}
-        name={tool}
-      />
-    ));
-  };
+      // update state
+      newSettings.length
+        ? setProjectData(data.filter((item) => newSettings.includes(item.tag)))
+        : setProjectData(data);
+
+      setSettings(newSettings);
+    },
+    [data, settings]
+  );
 
   return (
     <ProjectsContainer
@@ -68,19 +52,30 @@ export const Projects = ({ data }) => {
       animate="enter"
       exit="exit"
       variants={variants}
-      layout
     >
       <Header>Projects</Header>
       <ButtonContainer>
-        <FlexContainer>{filterButtons()}</FlexContainer>
+        <FlexContainer>
+          {tools.map((tool, idx) => (
+            <FilterButton key={idx} filter={filter} name={tool} />
+          ))}
+        </FlexContainer>
         <SortButton onClick={shuffle}>
           <DevelopmentIcon name={"Shuffle"} size={"3em"} />
         </SortButton>
       </ButtonContainer>
       <OuterProjectContainer>
-        <Flipper flipKey={projectData} spring={"gentle"}>
-          <ProjectContainer>{projects}</ProjectContainer>
-        </Flipper>
+        <ProjectContainer>
+          {projectData.map((project) => (
+            <Div
+              layout
+              transition={{ type: "spring", bounce: 0.3 }}
+              key={project.id}
+            >
+              <Project projectData={project} key={project.id} />
+            </Div>
+          ))}
+        </ProjectContainer>
       </OuterProjectContainer>
     </ProjectsContainer>
   );
