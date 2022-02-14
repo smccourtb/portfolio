3371779fa8c388d96db8e4a3e3b0d52f602e9abb
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-02-14 10:57:42 -0500

    convert animations to use framer-motion library

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,55 +1,94 @@
-import Slide from "./aboutMe/Slide";
 import { Container, Header, variants } from "../styles/global/GlobalStyles";
-
-import { DevelopmentIcon } from "./projects/DevelopmentIcon";
 import {
-  AnimContainer,
-  Content,
   ContentContainer,
+  ContentGrid,
+  ContentGridContentContainer,
   Paragraph,
-  Row,
   Snippet,
   Strikethrough,
-  TextBorder,
 } from "../styles/about-styles";
+import { motion } from "framer-motion";
+import { Icon } from "@iconify/react";
 
 const techIconData = [
-  { name: "Javascript", size: "3em" },
-  { name: "HTML 5", size: "1em" },
-  { name: "CSS 3", size: "1em" },
-  { name: "React", size: "2em" },
-  { name: "Styled Components", size: "1.5em" },
-  { name: "Webstorm", size: "1.5em" },
-  { name: "Webpack", size: "1.5em" },
-  { name: "Git", size: "1.5em" },
-  { name: "Github", size: "1.5em" },
-  { name: "Trello", size: "1.5em" },
+  { name: "akar-icons:javascript-fill", size: "3em" },
+  { name: "simple-icons:html5", size: "1em" },
+  { name: "simple-icons:css3", size: "1em" },
+  { name: "akar-icons:react-fill", size: "2em" },
+  { name: "simple-icons:styledcomponents", size: "1.5em" },
+  { name: "simple-icons:webstorm", size: "1.5em" },
+  { name: "simple-icons:webpack", size: "1.5em" },
+  { name: "bxl:git", size: "1.5em" },
+  { name: "akar-icons:github-fill", size: "1.5em" },
+  { name: "fontisto:trello", size: "1.5em" },
 ];
 
 const hobbyIconData = [
-  { name: "Go for a Bike Ride", size: "1em" },
-  { name: "Play Guitar", size: "3em" },
-  { name: "Go Hiking", size: "1em" },
-  { name: "Play Board Games", size: "2em" },
-  { name: "Develop Video Games", size: "1.5em" },
+  { name: "bx:bx-cycling", size: "1em" },
+  { name: "fluent:guitar-24-filled", size: "3em" },
+  { name: "ic:baseline-hiking", size: "1em" },
+  { name: "fa-solid:dice-d20", size: "2em" },
+  { name: "dashicons:games", size: "1.5em" },
 ];
 
-export const AboutMe = () => {
-  const ranNum = (min, max) => {
-    return Math.random() * (max - min) + min;
-  };
+const ranNum = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
 
+const up = {
+  visible: (ranNum) => {
+    const xValue = ranNum(5, 10);
+    return {
+      translateY: ["100%", "-900%"],
+      translateX: [-xValue, xValue, -xValue],
+      transition: {
+        delay: ranNum(3, 10),
+        duration: 15,
+        repeat: Infinity,
+        translateX: {
+          duration: ranNum(3, 6),
+          type: "spring",
+          repeat: Infinity,
+        },
+      },
+    };
+  },
+};
+const down = {
+  visible: (ranNum) => {
+    const xValue = ranNum(5, 10);
+    return {
+      translateY: ["-100%", "900%"],
+      translateX: [-xValue, xValue, -xValue],
+      transition: {
+        delay: ranNum(3, 10),
+        duration: 15,
+        repeat: Infinity,
+        translateX: {
+          duration: ranNum(3, 6),
+          type: "spring",
+          repeat: Infinity,
+        },
+      },
+    };
+  },
+};
+
+export const AboutMe = () => {
   const floatingIcons = (iconList, direction) => {
-    return iconList.map((item, idx) => (
-      <AnimContainer
-        key={idx}
-        position={ranNum(-10, 10)}
-        delay={ranNum(1, 25)}
-        speed={ranNum(15, 25)}
-        down={direction}
+    return iconList.map((item) => (
+      <motion.p
+        key={item.name}
+        style={{ position: "absolute", left: `${ranNum(10, 90)}%` }}
+        animate={"visible"}
+        variants={direction === "up" ? up : down}
+        custom={ranNum}
+        inital={{
+          x: `${ranNum(-900, 90)}%`,
+        }}
       >
-        <DevelopmentIcon name={item.name} size={item.size} />
-      </AnimContainer>
+        <Icon width={item.size} icon={item.name} />
+      </motion.p>
     ));
   };
 
@@ -65,41 +104,69 @@ export const AboutMe = () => {
       <Header>About</Header>
 
       <ContentContainer>
-        <Content>
-          <Slide direction={"up"} delay={0.5}>
-            <Row>
-              <Slide direction={"left"} delay={1.2}>
-                <Paragraph first>
-                  Hi, I'm Shawn! I started down this path seeing all these
-                  amazing websites and getting{" "}
-                  <Strikethrough>jealous</Strikethrough> curious about how they
-                  were made. The world is challenging, exciting, frustrating and
-                  everything rolled into one. Every day is an opportunity to
-                  learn something new, and I love bringing new ideas to life.
-                </Paragraph>
-              </Slide>
-              <TextBorder right />
-              {floatingIcons(techIconData, false)}
-            </Row>
-          </Slide>
-          <Slide direction={"down"} delay={0.5}>
-            <Row>
-              {floatingIcons(hobbyIconData, true)}
+        <ContentGrid>
+          <ContentGridContentContainer>
+            <Paragraph
+              first={"true"}
+              animate={{ x: ["100%", "0%"] }}
+              transition={{ delay: 2 }}
+            >
+              Hi, I'm Shawn! I started down this path seeing all these amazing
+              websites and getting <Strikethrough>jealous</Strikethrough>{" "}
+              curious about how they were made. The world is challenging,
+              exciting, frustrating and everything rolled into one. Every day is
+              an opportunity to learn something new, and I love bringing new
+              ideas to life.
+            </Paragraph>
+          </ContentGridContentContainer>
+          <ContentGridContentContainer>
+            <motion.div
+              animate={{ y: ["100%", "0%"] }}
+              transition={{ delay: 1 }}
+              style={{
+                backgroundColor: "whitesmoke",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </ContentGridContentContainer>
+          <ContentGridContentContainer
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+            }}
+          >
+            {floatingIcons(techIconData, "up")}
+          </ContentGridContentContainer>
+          <ContentGridContentContainer>
+            {floatingIcons(hobbyIconData, "down")}
+          </ContentGridContentContainer>
+          <ContentGridContentContainer>
+            <motion.div
+              animate={{ y: ["-100%", "0%"] }}
+              transition={{ delay: 1 }}
+              style={{
+                backgroundColor: "whitesmoke",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </ContentGridContentContainer>
 
-              <TextBorder left />
-
-              <Slide direction={"right"} delay={1.2}>
-                <Paragraph>
-                  When i'm not working, you can find me trying to keep up with
-                  my 3 boys, playing board games with friends, jamming out on my
-                  acoustic-campfire style, or belly-laughing with my{" "}
-                  <Strikethrough>best friend</Strikethrough> wife. Life is busy
-                  but short, and I try to spend everyday improving myself.
-                </Paragraph>
-              </Slide>
-            </Row>
-          </Slide>
-        </Content>
+          <ContentGridContentContainer>
+            <Paragraph
+              animate={{ x: ["-100%", "0%"] }}
+              transition={{ delay: 2 }}
+            >
+              When i'm not working, you can find me trying to keep up with my 3
+              boys, playing board games with friends, jamming out on my
+              acoustic-campfire style, or belly-laughing with my{" "}
+              <Strikethrough>best friend</Strikethrough> wife. Life is busy but
+              short, and I try to spend everyday improving myself.
+            </Paragraph>
+          </ContentGridContentContainer>
+        </ContentGrid>
       </ContentContainer>
 
       <Snippet>
--- src/styles/about-styles.js
+++ src/styles/about-styles.js
@@ -1,45 +1,27 @@
-import styled, { keyframes } from "styled-components/macro";
+import styled from "styled-components/macro";
+import { motion } from "framer-motion";
 
-const FloatUp = keyframes`
-  0% {
-    transform: translateY(900%);
-  }
-  50% {
-    opacity: 100%;
-  }
-  100% {
-    transform: translateY(-200%);
-  }
-`;
-
-export const AnimContainer = styled.div`
-  animation: ${FloatUp} infinite linear;
-  animation-duration: ${(props) => `${props.speed}s`};
-  animation-delay: ${(props) => `${props.delay}s`};
-  animation-direction: ${({ down }) => down && "reverse"};
-  position: absolute;
-  opacity: 0;
-  z-index: 2;
-  left: ${({ position, down }) => (down ? `-50%` : `${position}em`)};
-  right: ${({ position, down }) => (down ? `${position}em` : `-50%`)};
-
-  :hover {
-    animation-play-state: paused;
-  }
-`;
-export const Paragraph = styled.p`
-  text-align: ${({ first }) => (!first ? "left" : "right")};
+export const Paragraph = styled(motion.p)`
+  text-align: ${({ first }) => (first ? "right" : "left")};
   font-size: 0.9em;
   align-self: center;
+  padding: ${({ first }) => (first ? `0 0.5em 0 0` : `0 0 0 0.5em`)};
+  height: 100%;
+  width: 100%;
 `;
 
-export const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-self: center;
+export const ContentGrid = styled(motion.div)`
+  display: grid;
   padding: 2em 1em;
   max-width: 700px;
   max-height: 600px;
+  grid-template-columns: 1fr 3px 1fr;
+  grid-template-rows: 1fr 1fr;
+`;
+
+export const ContentGridContentContainer = styled.div`
+  overflow: hidden;
+  position: relative;
 `;
 
 export const Strikethrough = styled.span`
@@ -56,19 +38,7 @@ export const Snippet = styled.span`
   padding: 0 3em;
 `;
 
-export const Row = styled.div`
-  display: flex;
-  height: 100%;
-`;
-
-export const TextBorder = styled.div`
-  width: 100%;
-  ${({ right }) => right && `border-left: .3em solid white`};
-  ${({ left }) => left && `border-right: .3em solid white`};
-  margin: ${({ left }) => (left ? `0 0.33em 0 0.5em` : `0 0.5em 0 0.4em`)};
-`;
-
-export const ContentContainer = styled(motion.div)`
+export const ContentContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
